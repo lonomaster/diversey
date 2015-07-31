7137e680d205de4b839f20c03f2164be01a63e6a
lonomaster <lonomaster@gmail.com>
2015-07-31 15:50:17 -0300

    comment test

--- app/src/main/java/com/diversey/servicio/logistica/UserItemAdapter.java
+++ app/src/main/java/com/diversey/servicio/logistica/UserItemAdapter.java
@@ -1,6 +1,7 @@
 package com.diversey.servicio.logistica;
 
 import android.content.Context;
+import android.graphics.Typeface;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,6 +20,9 @@ import java.util.List;
 public class UserItemAdapter extends ArrayAdapter<UserRecord> {
 	private List<UserRecord> users;
 	private Context context;
+	public Typeface BebasNeueRegular;
+	public Typeface BebasNeueLight;
+	public Typeface BebasNeueBold;
 	
 	
 	public UserItemAdapter(Context context, int textViewResourceId, List<UserRecord> users) {
@@ -26,6 +30,15 @@ public class UserItemAdapter extends ArrayAdapter<UserRecord> {
 		this.context = context;
 		this.users = users;
 		Collections.reverse(this.users);
+
+		BebasNeueLight = Typeface.createFromAsset(context.getAssets(),
+				"BebasNeueLight.ttf");
+
+		BebasNeueRegular = Typeface.createFromAsset(context.getAssets(),
+				"BebasNeueRegular.ttf");
+
+		BebasNeueBold = Typeface.createFromAsset(context.getAssets(),
+				"BebasNeueBold.ttf");
 	}
 
 	@Override
@@ -45,14 +58,35 @@ public class UserItemAdapter extends ArrayAdapter<UserRecord> {
 		UserRecord user = users.get(position);
 		Log.d("SugarContent", user.toString());
 		if (user != null) {
+
+
+
+
+
+			TextView title_site = (TextView) v.findViewById(R.id.title_site);
 			TextView username = (TextView) v.findViewById(R.id.list_nombre);
 			TextView razonSocial = (TextView) v.findViewById(R.id.list_nombre_empresa);
+
 			TextView contacto = (TextView) v.findViewById(R.id.list_nombre_contacto);
+			TextView title_contacto = (TextView) v.findViewById(R.id.title_contacto);
+
+
+
 			TextView direccion = (TextView) v.findViewById(R.id.list_direccion_empresa);
+			TextView title_direccion = (TextView) v.findViewById(R.id.title_direccion);
+
+
 			ImageView state = (ImageView) v.findViewById(R.id.list_estado);
 			ImageView logo = (ImageView)v.findViewById(R.id.list_avatar);
 
 			TextView otId= (TextView) v.findViewById(R.id.list_numero_ot);
+			TextView title_ot_id= (TextView) v.findViewById(R.id.title_ot_id);
+
+
+
+			TextView title_fecha= (TextView) v.findViewById(R.id.title_fecha);
+
+
 			TextView fechaAsignacion= (TextView) v.findViewById(R.id.list_fecha_asignacion);
 			TextView fechaEjecucion= (TextView) v.findViewById(R.id.list_fecha_ejecucion);
 			fechaAsignacion.setVisibility(View.GONE);
@@ -67,14 +101,31 @@ public class UserItemAdapter extends ArrayAdapter<UserRecord> {
 
 			//Picasso.with(context).setIndicatorsEnabled(true);
 
-			razonSocial.setText(user.razon_social);
+			username.setTypeface(BebasNeueRegular);
 			username.setText(user.nombre);
-			contacto.setText(user.nombre_contacto);
+
+			title_site.setTypeface(BebasNeueBold);
+			razonSocial.setText(user.razon_social);
+			razonSocial.setTypeface(BebasNeueRegular);
+
+			title_direccion.setTypeface(BebasNeueBold);
 			direccion.setText(user.direccion);
+			direccion.setTypeface(BebasNeueRegular);
+
+			title_contacto.setTypeface(BebasNeueBold);
+			contacto.setText(user.nombre_contacto);
+			contacto.setTypeface(BebasNeueRegular);
+
+			title_ot_id.setTypeface(BebasNeueBold);
 			otId.setText(user.idot);
+			otId.setTypeface(BebasNeueRegular);
+
+			title_fecha.setTypeface(BebasNeueBold);
 			fechaAsignacion.setText(user.fecha_creacion);
+			fechaAsignacion.setTypeface(BebasNeueRegular);
 			fechaEjecucion.setText(user.fecha_ejecucion);
-			
+			fechaEjecucion.setTypeface(BebasNeueRegular);
+
 			if(state != null){
 				int st = Integer.valueOf(user.tipo_orden_id);
 				switch (st) {
